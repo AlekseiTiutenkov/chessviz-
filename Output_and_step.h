a99598b50802c3704ef73e35eded4df3022ed635
AlekseiTuitenkov <AlekseiTiutenkov>
2020-04-12 20:30:58 +0700

    Created a header file for include guard and removed function prototypes from the main file

---
+++ Output_and_step.h
@@ -0,0 +1,5 @@
+#ifndef BOARD_AND_STEP_H
+#define BOARD_AND_STEP_H
+void step(char A[9][9], int n);
+void Board(char A[9][9], int n);
+#endif
--- main.c
+++ main.c
@@ -1,13 +1,12 @@
+#include "Output_and_step.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-void Board(char A[9][9], int n);
-void step(char A[9][9], int n);
 int main()
 {
     char A[9][9];
     Board(A, 9);
-    step(A,9);
+    step(A, 9);
     system("Pause");
     return 0;
 }
