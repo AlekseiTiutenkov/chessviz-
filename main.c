281e566721e8517747c0bb36f27bee8fd723d874
AlekseiTuitenkov <AlekseiTiutenkov>
2020-04-12 21:38:53 +0700

    code style check

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include "Output_and_step.h"
-#include <stdio.h>
-#include <stdlib.h>
+	#include <stdio.h>
+		#include <stdlib.h>
 
-int main()
+		int main()
 {
-    char A[9][9];
+   			 char A[9][9];
     Board(A, 9);
-    step(A, 9);
+  	 step(A, 9);
     system("Pause");
     return 0;
 }
